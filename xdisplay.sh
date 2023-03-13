80028c1c13938fdba8473e8be11c206b073e66f2
Sudarshan Kakoty <52618169+fr0xk@users.noreply.github.com>
2023-03-13 11:11:59 +0530

    Create xdisplay.sh

---
+++ xdisplay.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Define required packages
+
+required_packages="x11-repo xorg-server xorg-xsetroot xorg-xrandr xterm mpv xorg-xvfb aterm"
+
+# Update the package list and install necessary packages
+
+echo "Installing required packages..."
+
+for package in $required_packages; do
+
+    pkg install "$package"
+
+done
+
+# Set up the X session
+
+echo "Starting X session..."
+
+screen_size=$(termux-info | grep "Screen size" | awk '{print $3}')
+
+Xvfb :1 -screen 0 "${screen_size}x768" -ac &
+
+export DISPLAY=:1
+
+xsetroot -solid "#000000"
+
+xterm &
+
+
+
+
