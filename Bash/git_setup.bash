69a9278a9282ed7a1cf1bf8bd3493864333ccda1
fr0xk <nikolaibitinit@gmail.com>
2023-07-20 13:07:58 +0530

    changed git setup to setup ssh

--- Bash/git_setup.bash
+++ Bash/git_setup.bash
@@ -1,53 +1,17 @@
 #!/bin/bash
 
-# Check if SSH is installed and set up
-if [ -z "$(which ssh)" ]; then
-  echo "SSH is not installed. Please install and set it up before running this script."
-  exit 1
-fi
+# Clone the repository using HTTPS.
+# Generate an SSH key pair.
+ssh-keygen -t rsa -b 4096
 
-# Check if GPG is installed and set up
-if [ -z "$(which gpg)" ]; then
-  echo "GPG is not installed. Please install and set it up before running this script."
-  exit 1
-fi
+# Add the public key to your GitHub account.
+cat ~/.ssh/id_rsa.pub | ssh fr0xk@github.com 'cat >> ~/.ssh/authorized_keys'
 
-# Set user identity for Git
-echo "Please enter your email address for Git:"
-read git_email
-echo "Please enter your name for Git:"
-read git_name
-git config --global user.email "$git_email"
-git config --global user.name "$git_name"
-
-# Set up SSH authentication
-if [ -f "$HOME/.ssh/id_rsa" ]; then
-  echo "SSH key already exists. Skipping SSH key generation."
-else
-  echo "Generating new SSH key..."
-  ssh-keygen -t rsa -b 4096 -C "$git_email"
-fi
-
-echo "Adding SSH key to SSH agent..."
+# Start the SSH agent and add your private key to it.
 eval "$(ssh-agent -s)"
-ssh-add "$HOME/.ssh/id_rsa"
+ssh-add -k ~/.ssh/id_rsa
 
-# Set up GPG authentication
-echo "Exporting GPG key..."
-gpg --armor --export "$git_email" > "$HOME/gpgkey.txt"
-
-echo "Adding GPG key to Git..."
-git config --global user.signingkey "$(gpg --list-secret-keys --keyid-format LONG | awk '$1 == "sec:" {print $2}' | cut -d '/' -f 2)"
-git config --global commit.gpgsign true
-
-# Clone a test repository to verify setup
-echo "Cloning test repository to verify setup..."
-git clone git@github.com:<username>/<repository>.git
-cd <repository>
-echo "Hello, world!" > hello.txt
-git add hello.txt
-git commit -m "Test commit"
-git push
-
-echo "Setup complete."
+# Add the SSH key to your SSH config permanently.
+echo "Host github.com
+  IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
 
