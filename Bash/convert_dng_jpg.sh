17ce58dde90e9cc95ace779eb12f5929cd8f20b3
Sudarshan Kakoty <52618169+fr0xk@users.noreply.github.com>
2024-01-05 14:43:37 +0530

    Update convert_dng_jpg.sh
    Updated with explanation of command arguments used in dcraw

--- Bash/convert_dng_jpg.sh
+++ Bash/convert_dng_jpg.sh
@@ -29,10 +29,21 @@ for input_file in "$@"; do
     filename=$(basename -- "$input_file")
     filename_noext="${filename%.*}"
 
-    # Convert DNG to JPEG with color balance and noise reduction
+    # Convert DNG to JPEG with color balance and noise reduction using dcraw
+    # -c: Output to stdout
+    # -w: Use camera white balance
+    # -q 3: Set quality to highest
+    # -o 2: Apply Auto level stretch
+    # -T: Write 16-bit TIFF
+    # -H 5: Set chromatic noise reduction to 5
+    # -W: Enable luminance noise reduction
+    # -b 2.2: Set color balance
     dcraw -c -w -q 3 -o 2 -T -H 5 -W -b 2.2 "$input_file" > "${filename_noext}.ppm"
 
     if [ $? -eq 0 ]; then
+        # Convert the intermediate PPM to JPEG with imagemagick
+        # -colorspace sRGB: Ensure the output is a color image, not black and white
+        # -quality 100: Set JPEG quality to the highest
         convert -colorspace sRGB -quality 100 "${filename_noext}.ppm" "${filename_noext}.jpg"
 
         # Optional: Delete intermediate PPM file
